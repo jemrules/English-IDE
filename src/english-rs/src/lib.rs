a56b725359057ebd320640cbe30dd8ffdfaa949d
jemrules <evern.mccullough@comcast.net>
2024-03-25 23:25:28 -0600

    Stuff

--- src/english-rs/src/lib.rs
+++ src/english-rs/src/lib.rs
@@ -2,11 +2,13 @@ use pyo3::prelude::*;
 
 mod neural_network;
 
+use crate::neural_network::net;
 use crate::neural_network::neuron;
 use crate::neural_network::active_funcs::get_from_string;
 use crate::neural_network::active_funcs::get_deriv_from_string;
 
 // "../../.venv/Scripts/activate.bat"
+// maturin develop
 
 /// Formats the sum of two numbers as string.
 // #[pyfunction]
@@ -15,17 +17,22 @@ use crate::neural_network::active_funcs::get_deriv_from_string;
 // }
 #[pyfunction]
 fn test_neuron(q_weights: Vec<f32>, q_bias: f32, q_activation: String) -> PyResult<String> {
-    let mut n = neuron {
-        weights: q_weights,
-        bias: q_bias,
-        activation: get_from_string(&q_activation),
-        activation_deriv: get_deriv_from_string(&q_activation),
-        value: 0.0,
-        t_len: 0,
-        w_derivs: vec![0.0],
-        b_deriv: 0.0,
+    // let mut n = neuron {
+    //     weights: q_weights,
+    //     bias: q_bias,
+    //     activation: get_from_string(&q_activation),
+    //     activation_deriv: get_deriv_from_string(&q_activation),
+    //     value: 0.0,
+    //     t_len: 0,
+    //     w_derivs: vec![0.0],
+    //     b_deriv: 0.0,
+    // };
+    let mut netw = net::network {
+        shape: vec![(0,"linear".to_string()),(0,"sigmoid".to_string())],
+        model: Vec::new(),
     };
-    Ok(("Activation: ").to_string())
+    netw.construct_shape();
+    Ok((netw.shape.iter().map(|a| a.1.clone()).collect::<Vec<String>>().join(" ")).to_string())
 }
 
 
