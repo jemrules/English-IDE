bd10f9b6ab959f9c1f965d74dd6accb2e4de4212
jemrules <evern.mccullough@comcast.net>
2024-04-15 07:58:00 -0600

    test

--- src/EngIDE.py
+++ src/EngIDE.py
@@ -5,82 +5,89 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from html import escape
 from syntax_handle import colorize
+rust=False
+try:
+    import english_rs as Ers
+    rust=True
+except ImportError:
+    rust=False
+    print("Rust end not compiled!")
 
 def rgb_txt(text,rgb):
-	return "<span style='color:rgb({R},{G},{B})'>{txt}</span>".format(R=rgb[0],G=rgb[1],B=rgb[2],txt=escape(text))
+    return "<span style='color:rgb({R},{G},{B})'>{txt}</span>".format(R=rgb[0],G=rgb[1],B=rgb[2],txt=escape(text))
 def css_txt(text,css):
-	return "<span style='{css}'>{txt}</span>".format(css=css,txt=escape(text))
+    return "<span style='{css}'>{txt}</span>".format(css=css,txt=escape(text))
 
 class window_widget(QDockWidget):
-	def __init__(self):
-		super().__init__()
-		# self.setObjectName("WindowWidget")
-		self.setWindowTitle('Window Widget')
-		self.setWidget(QWidget())
-		self.layout=QVBoxLayout()
-		self.widget().setLayout(self.layout)
-		# self.layout.addWidget(QLabel('Window Widget'))
+    def __init__(self):
+        super().__init__()
+        # self.setObjectName("WindowWidget")
+        self.setWindowTitle('Window Widget')
+        self.setWidget(QWidget())
+        self.layout=QVBoxLayout()
+        self.widget().setLayout(self.layout)
+        # self.layout.addWidget(QLabel('Window Widget'))
 class text_box(QTextEdit):
-	def __init__(self):
-		super().__init__()
-		self.setAcceptRichText(False)
-		self.setObjectName("text_box")
-		self.textChanged.connect(self.update_text)
-		self.cursorPositionChanged.connect(self.cursor_pos_update)
-		self.disableChange=0
-		self.currentPos=1
-		self.last=""
-		self.setLineWrapMode(QTextEdit.NoWrap)
-	def update_text(self):
-		if self.disableChange>0:
-			self.disableChange-=1
-			return
-		self.disableChange=3
-		self.currentPos=self.textCursor().position()
-		colorized=colorize(self.toPlainText(),self.currentPos)
-		if self.last==colorized[0]:
-			return
-		self.setHtml(colorized[0])
-		self.last=colorized[0]
-		cursor=self.textCursor()
-		cursor.setPosition(self.currentPos+colorized[1])
-		self.setTextCursor(cursor)
-	def cursor_pos_update(self):
-		self.update_text()
+    def __init__(self):
+        super().__init__()
+        self.setAcceptRichText(False)
+        self.setObjectName("text_box")
+        self.textChanged.connect(self.update_text)
+        self.cursorPositionChanged.connect(self.cursor_pos_update)
+        self.disableChange=0
+        self.currentPos=1
+        self.last=""
+        self.setLineWrapMode(QTextEdit.NoWrap)
+    def update_text(self):
+        if self.disableChange>0:
+            self.disableChange-=1
+            return
+        self.disableChange=3
+        self.currentPos=self.textCursor().position()
+        colorized=colorize(self.toPlainText(),self.currentPos)
+        if self.last==colorized[0]:
+            return
+        self.setHtml(colorized[0])
+        self.last=colorized[0]
+        cursor=self.textCursor()
+        cursor.setPosition(self.currentPos+colorized[1])
+        self.setTextCursor(cursor)
+    def cursor_pos_update(self):
+        self.update_text()
 
 class text_area(QWidget):
-	def __init__(self,name="text_area"):
-		super().__init__()
-		self.layout=QVBoxLayout()
-		# self.setObjectName("test")
-		self.setLayout(self.layout)
-		self.text_box=text_box()
-		self.layout.addWidget(self.text_box)
+    def __init__(self,name="text_area"):
+        super().__init__()
+        self.layout=QVBoxLayout()
+        # self.setObjectName("test")
+        self.setLayout(self.layout)
+        self.text_box=text_box()
+        self.layout.addWidget(self.text_box)
 
 class suggestion_area(QWidget):
-	def __init__(self):
-		super().__init__()
-		# self.setObjectName("SuggestionArea")
-		self.layout=QVBoxLayout()
-		self.setLayout(self.layout)
-		self.layout.addWidget(QLabel('Suggestion Area'))
+    def __init__(self):
+        super().__init__()
+        # self.setObjectName("SuggestionArea")
+        self.layout=QVBoxLayout()
+        self.setLayout(self.layout)
+        self.layout.addWidget(QLabel('Suggestion Area'))
 
 class GUI(QMainWindow):
-	def __init__(self):
-		super().__init__()
-		self.initUI()
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-	def initUI(self):
-		self.setWindowTitle('GUI')
-		self.setGeometry(100, 100, 800, 600)
-		self.setCentralWidget(QWidget())
-		self.layout=QVBoxLayout()
-		self.centralWidget().setLayout(self.layout)
-		self.text_areas=QTabWidget()
-		self.text_areas.setObjectName("text_areas")
-		self.layout.addWidget(self.text_areas)
-		self.text_areas.addTab(text_area('tab_1'),'Tab 1')
-		self.text_areas.addTab(text_area('tab_2'),'Tab 2')
+    def initUI(self):
+        self.setWindowTitle('GUI')
+        self.setGeometry(100, 100, 800, 600)
+        self.setCentralWidget(QWidget())
+        self.layout=QVBoxLayout()
+        self.centralWidget().setLayout(self.layout)
+        self.text_areas=QTabWidget()
+        self.text_areas.setObjectName("text_areas")
+        self.layout.addWidget(self.text_areas)
+        self.text_areas.addTab(text_area('tab_1'),'Tab 1')
+        self.text_areas.addTab(text_area('tab_2'),'Tab 2')
 app=QApplication(sys.argv)
 f=open("style.css","r")
 app.setStyleSheet(f.read())
--- src/english-rs/src/lib.rs
+++ src/english-rs/src/lib.rs
@@ -18,31 +18,34 @@ use crate::neural_network::active_funcs::get_deriv_from_string;
 //     Ok((a + b).to_string())
 // }
 
-#[pyfunction]
-fn test_neuron(q_weights: Vec<f32>, q_bias: f32, q_activation: String) -> PyResult<String> {
-    // let mut n = neuron {
-    //     weights: q_weights,
-    //     bias: q_bias,
-    //     activation: get_from_string(&q_activation),
-    //     activation_deriv: get_deriv_from_string(&q_activation),
-    //     value: 0.0,
-    //     t_len: 0,
-    //     w_derivs: vec![0.0],
-    //     b_deriv: 0.0,
-    // };
-    let mut netw = net::network {
-        shape: vec![(5,"linear".to_string()),(5,"sigmoid".to_string())],
-        model: Vec::new(),
-    };
-    netw.model=netw.construct_shape();
-    Ok((netw.shape.iter().map(|a| a.1.clone()).collect::<Vec<String>>().join(" ")).to_string())
-}
-
-/// A Python module implemented in Rust.
 #[pymodule]
-fn nn(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(test_neuron, m)?)?;
+fn auto_features(_py: Python,m: &PyModule) -> PyResult<()> {
     Ok(())
 }
+
+// #[pyfunction]
+// fn test_neuron(q_weights: Vec<f32>, q_bias: f32, q_activation: String) -> PyResult<String> {
+//     // let mut n = neuron {
+//     //     weights: q_weights,
+//     //     bias: q_bias,
+//     //     activation: get_from_string(&q_activation),
+//     //     activation_deriv: get_deriv_from_string(&q_activation),
+//     //     value: 0.0,
+//     //     t_len: 0,
+//     //     w_derivs: vec![0.0],
+//     //     b_deriv: 0.0,
+//     // };
+//     let mut netw = net::network {
+//         shape: vec![(5,"linear".to_string()),(5,"sigmoid".to_string())],
+//         model: Vec::new(),
+//     };
+//     netw.model=netw.construct_shape();
+//     Ok((netw.shape.iter().map(|a| a.1.clone()).collect::<Vec<String>>().join(" ")).to_string())
+// }
+// #[pymodule]
+// fn nn(_py: Python, m: &PyModule) -> PyResult<()> {
+//     m.add_function(wrap_pyfunction!(test_neuron, m)?)?;
+//     Ok(())
+// }
 
 //  //          println!("{}",model.len());
